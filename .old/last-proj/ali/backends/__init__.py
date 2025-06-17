84142f72d4e902e8c4247a4c4698049183ca2630
Avid Coder <avidcoderr@gmail.com>
2025-06-16 23:24:04 -0400

    clean up

--- .old/last-proj/ali/backends/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Backend interfaces for different LLM providers."""
-
-from .base import BaseBackend
-from .mlx_backend import MLXBackend
-from .ollama_backend import OllamaBackend
-
-__all__ = ["BaseBackend", "MLXBackend", "OllamaBackend"]
